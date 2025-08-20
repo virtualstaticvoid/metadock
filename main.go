a8582affca0675a5a9cb32d2cdafd6a059b4ce9c
Chris Stefano <virtualstaticvoid@gmail.com>
2025-08-20 11:45:53 +0200

    retrieve session credential at startup
    retrieving the credential on-demand, in the `roleCredentialsHandler`, takes too long and AWS SDK/CLI requests just timeout

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/gorilla/mux"
 )
 
@@ -25,7 +26,8 @@ const Version = "0.0.0"
 type IMDSv2Service struct {
 	Profile       string
 	Configuration aws.Config
-	FakeToken     string
+	ServiceToken  string
+	Credential    aws.Credentials
 }
 
 const DefaultPort = "8080"
@@ -64,6 +66,49 @@ func generateToken(length int) (string, error) {
 	return base64.URLEncoding.EncodeToString(bytes), nil
 }
 
+func loadCredential(cfg aws.Config) (aws.Credentials, error) {
+	credential, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	return credential, nil
+}
+
+func createTemporaryCredential(cfg aws.Config) (aws.Credentials, error) {
+	stsClient := sts.NewFromConfig(cfg)
+	resp, err := stsClient.GetSessionToken(context.TODO(), &sts.GetSessionTokenInput{})
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	cred := resp.Credentials
+
+	return aws.Credentials{
+		Source:          "createTemporaryCredentials",
+		AccessKeyID:     *cred.AccessKeyId,
+		SecretAccessKey: *cred.SecretAccessKey,
+		SessionToken:    *cred.SessionToken,
+		Expires:         *cred.Expiration,
+		CanExpire:       true,
+	}, nil
+}
+
+func loadOrCreateCredential(cfg aws.Config) (aws.Credentials, error) {
+	credential, err := loadCredential(cfg)
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+
+	// issue temporary credential
+	if !credential.CanExpire {
+		credential, err = createTemporaryCredential(cfg)
+		if err != nil {
+			return aws.Credentials{}, err
+		}
+	}
+
+	return credential, nil
+}
+
 type loggingResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
@@ -92,16 +137,19 @@ func loggingMiddleware(next http.Handler) http.Handler {
 		)
 	})
 }
+
+// service entrypoint
+
 func main() {
-	versionFlag := flag.Bool("version", false, "Print the version and exit")
+	versionFlag := flag.Bool("version", false, "Print the version and exit.")
 	flag.Usage = func() {
 		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [aws-profile]\n\n", os.Args[0])
-		fmt.Println("If [aws-profile] is provided, or the AWS_PROFILE environment variable is set, the app runs with that profile.")
+		fmt.Println("If \"aws-profile\" is provided, or the AWS_PROFILE environment variable is set, the app runs with that profile.")
 	}
 	flag.Parse()
 
 	if *versionFlag {
-		fmt.Println("Version:", Version)
+		fmt.Printf("Version: %s\n", Version)
 		return
 	}
 
@@ -121,22 +169,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to load AWS configuration: %v\n", err)
 	}
+	log.Printf("Configured for %q AWS profile.\n", profile)
 
-	fakeToken, err := generateToken(32)
+	serviceToken, err := generateToken(32)
 	if err != nil {
 		log.Fatalf("Unable to generate token: %v\n", err)
 	}
 
+	// retrieve the credential at startup, as it takes too long if retrieved within
+	// the roleCredentialsHandler function, since the callee has a very short TTFB timeout
+	log.Printf("Retrieving credentials...\n")
+	credential, err := loadOrCreateCredential(configuration)
+	if err != nil {
+		log.Fatalf("Unable to load or create temporary credential: %v\n", err)
+	}
+	log.Printf("Retrieved credentials, expires at %q.\n", credential.Expires)
+
+	// setup service, middleware and router
 	service := &IMDSv2Service{
 		Profile:       profile,
 		Configuration: configuration,
-		FakeToken:     fakeToken,
+		ServiceToken:  serviceToken,
+		Credential:    credential,
 	}
-
-	// FIXME: cache credentials
-	_, _ = service.loadCredentials()
-
-	log.Printf("Configured for %q AWS profile.\n", service.Profile)
 
 	router := mux.NewRouter()
 	router.Use(loggingMiddleware)
@@ -147,13 +202,8 @@ func main() {
 	router.HandleFunc("/latest/meta-data/iam/security-credentials/{role}", service.roleCredentialsHandler).Methods("GET")
 	router.HandleFunc("/health/", service.healthHandler).Methods("GET")
 
+	// setup HTTP server
 	port := getEnvOrDefault("PORT", DefaultPort)
-	log.Printf("Listening on all interfaces on port %s.\n", port)
-
-	// context for background processing
-	// which traps SIGINT/SIGTERM
-	ctx := contextWithSignal(context.Background())
-
 	server := &http.Server{
 		Addr:         ":" + port,
 		Handler:      router,
@@ -161,18 +211,22 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 	}
 
+	// context for background processing
+	ctx := contextWithSignal(context.Background())
+
 	// run the server in a goroutine so it doesn’t block
 	go func() {
+		log.Printf("Listening on port %s for all network interfaces.\n", port)
 		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.Fatalf("Server error: %v\n", err)
 		}
 	}()
 
-	// wait for signal (ctx.done)
+	// wait for SIGINT/SIGTERM
 	<-ctx.Done()
 	log.Println("Shutting down server...")
 
-	// create a context with timeout for shutdown
+	// context with timeout for shutdown
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
@@ -183,14 +237,7 @@ func main() {
 	}
 }
 
-func (svc *IMDSv2Service) loadCredentials() (aws.Credentials, error) {
-	credentials, err := svc.Configuration.Credentials.Retrieve(context.TODO())
-	if err != nil {
-		log.Printf("Failed to retrieve credentials: %v\n", err)
-		return aws.Credentials{}, err
-	}
-	return credentials, nil
-}
+// IDMS implementation
 
 // url: / (root)
 func (svc *IMDSv2Service) rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -203,13 +250,13 @@ func (svc *IMDSv2Service) rootHandler(w http.ResponseWriter, r *http.Request) {
 func (svc *IMDSv2Service) tokenHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, _ = fmt.Fprintf(w, svc.FakeToken)
+	_, _ = fmt.Fprintf(w, svc.ServiceToken)
 }
 
 // url: /latest/meta-data/iam/security-credentials/
 func (svc *IMDSv2Service) credentialsHandler(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("X-aws-ec2-metadata-token")
-	if token != svc.FakeToken {
+	if token != svc.ServiceToken {
 		log.Printf("Token mismatched error.\n")
 		http.Error(w, "", http.StatusForbidden)
 		return
@@ -217,40 +264,34 @@ func (svc *IMDSv2Service) credentialsHandler(w http.ResponseWriter, r *http.Requ
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, _ = fmt.Fprintf(w, "proxy-role")
+	_, _ = fmt.Fprintf(w, "metadock")
 }
 
 // url: /latest/meta-data/iam/security-credentials/{role}
 func (svc *IMDSv2Service) roleCredentialsHandler(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	//role := vars["role"]
+	// NB: this handler has to be very fast!
 
 	token := r.Header.Get("X-aws-ec2-metadata-token")
-	if token != svc.FakeToken {
+	if token != svc.ServiceToken {
 		log.Printf("Token mismatched error.\n")
 		http.Error(w, "", http.StatusForbidden)
-		return
-	}
-
-	// FIXME: use cached credentials
-	credentials, err := svc.loadCredentials()
-	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
+	credential := svc.Credential
 	response := map[string]string{
 		"Code":            "Success",
 		"LastUpdated":     time.Now().Format(time.RFC3339),
 		"Type":            "AWS-HMAC",
-		"AccessKeyId":     credentials.AccessKeyID,
-		"SecretAccessKey": credentials.SecretAccessKey,
-		"Token":           credentials.SessionToken,
-		"Expiration":      credentials.Expires.Format(time.RFC3339),
+		"AccessKeyId":     credential.AccessKeyID,
+		"SecretAccessKey": credential.SecretAccessKey,
+		"Token":           credential.SessionToken,
+		"Expiration":      credential.Expires.Format(time.RFC3339),
 	}
+
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		log.Printf("roleCredentialsHandler encode error: %v\n", err)
 		http.Error(w, "", http.StatusInternalServerError)
